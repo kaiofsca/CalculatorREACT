c783ff8a5e6868e78d896d4f881c400f8ec78e2a
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-08-31 12:15:57 -0300

    terminando estilização

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,32 +1,38 @@
 
 import React from 'react'
 import './Calculator.css'
-import { Container } from '@mui/material'; 
+import { Box, Container } from '@mui/material'; 
 
 export default function Calculator() {
   return (
-    <Container maxWidth='xs'>
-        <div className='wrapper'>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>,</button>
+    <div>
+      <Box m={5}/>
+      <Container maxWidth='xs'>
+          <div className='wrapper'>
+          <Box m={8}/>
+            <h1 className='result'>01</h1>
             <button>AC</button>
             <button>+/-</button>
             <button>%</button>
-            <button>/</button>
-            <button>X</button>
-            <button>-</button>
-            <button>+-</button>
+            <button className='orange'>/</button>
+            <button className='gray'>7</button>
+            <button className='gray'>8</button>
+            <button className='gray'>9</button>
+            <button className='orange'>X</button>
+            <button className='gray'>4</button>
+            <button className='gray'>5</button>
+            <button className='gray'>6</button>
+            <button className='orange'>-</button>
+            <button className='gray'>1</button>
+            <button className='gray'>2</button>
+            <button className='gray'>3</button>
+            <button className='orange'>+</button>
+            <button className='gray'>0</button>
+            <button className='gray'>,</button>
+            <button className='gray' style={{visibility : 'hidden'}}>,</button>
             <button>=</button>
-        </div>
-    </Container>
+          </div>
+      </Container>
+    </div>
   )
 }
